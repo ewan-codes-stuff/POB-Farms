0ce504d1c3b4864e31fd81688e88d8cc9676abee
ewan-codes-stuff <71463995+ewan-codes-stuff@users.noreply.github.com>
2024-02-29 19:06:32 +0000

    Enemies spawn on inaccessible terrain and move onto the map

--- Assets/Scripts/Ewan/EnemySpawner.cs
+++ Assets/Scripts/Ewan/EnemySpawner.cs
@@ -57,22 +57,22 @@ public class EnemySpawner : MonoBehaviour
         switch (spawnSide) 
         {
             case 1:
-                xSpawn = 5;
+                xSpawn = 6;
                 xRandomised = true;
                 zRandomised = false;
                 break;
             case 2:
-                xSpawn = -6;
+                xSpawn = -7;
                 xRandomised = true;
                 zRandomised = false;
                 break;
             case 3:
-                zSpawn = 5;
+                zSpawn = 6;
                 zRandomised = true;
                 xRandomised = false;
                 break;
             case 4:
-                zSpawn = -6;
+                zSpawn = -7;
                 zRandomised = true;
                 xRandomised = false;
                 break;
@@ -82,50 +82,51 @@ public class EnemySpawner : MonoBehaviour
         
         Debug.Log("Spawned Enemies");
         spawnTimer -= 1;
-        if (GameManager.instance.aiManager.IsNight())
+        if (GameManager.instance.aiManager.IsNight()) //Check if night
         {
-            
-            for(int m = spawnBudget; m>0; m -= enemyTax)
+            for(int m = spawnBudget; m>0; m -= enemyTax) //Loop until you've spawned all the enemies for this round
             {
-                int randomiseSpawnCounter = 0;
-                enemyIDCounter += 1;
-                int randomNum = Random.Range(0, 11);
+                int randomiseSpawnCounter = 0; //Counter to prevent infinite while loops by setting a limit (which is currently 100 randomisations)
+                enemyIDCounter += 1; //To name the enemy in the heirachy
+                int randomNum = Random.Range(0, 11); 
                 if (xRandomised) 
                 { 
-                    while(GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].entity != null) 
+                    while(GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].entity != null) //If the proposed spawning location is occupied by an entity,
+                                                                                                                //randomise for a new spawn
                     {
                         randomNum = Random.Range(0, 11);
                         randomiseSpawnCounter += 1;
-                        if(randomiseSpawnCounter > 100)
+                        if(randomiseSpawnCounter > 100)     //If the spawn counter has gone over 100, spawn 1 closer to the centre until we reach 0 or find a space
                         {
                             if(xSpawn > 0) { xSpawn--; randomiseSpawnCounter = 0; }
                             if(xSpawn < 0) { xSpawn++; randomiseSpawnCounter = 0; }
                             if(xSpawn == 0) { break; }
                         }
                     }
-                    if (GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].entity == null)
+                    if (GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].entity == null) //If there is nothing there, spawn an enemy, otherwise do nothing
                     {
                         SpawnEnemy(xSpawn, randomNum - 6);
-                        hasSpawnedEnemiesTonight = true;
+                        hasSpawnedEnemiesTonight = true; //Boolean to check that something has been spawned in the round. For the AIManager
                     }
                 }
                 else 
                 {
-                    while (GameManager.instance.tileArray[new Vector2Int(randomNum - 6, zSpawn)].entity != null)
+                    while (GameManager.instance.tileArray[new Vector2Int(randomNum - 6, zSpawn)].entity != null)//If the proposed spawning location is occupied by an entity,
+                                                                                                                //randomise for a new spawn
                     {
                         randomNum = Random.Range(0, 11);
                         randomiseSpawnCounter += 1;
-                        if (randomiseSpawnCounter > 100)
+                        if (randomiseSpawnCounter > 100)    //If the spawn counter has gone over 100, spawn 1 closer to the centre until we reach 0 or find a space
                         {
                             if (zSpawn > 0) { zSpawn--; randomiseSpawnCounter = 0; }
                             if (zSpawn < 0) { zSpawn++; randomiseSpawnCounter = 0; }
                             if (zSpawn == 0) { break; }
                         }
                     }
-                    if (GameManager.instance.tileArray[new Vector2Int(randomNum - 6, zSpawn)].entity == null)
+                    if (GameManager.instance.tileArray[new Vector2Int(randomNum - 6, zSpawn)].entity == null)   //If there is nothing there, spawn an enemy, otherwise do nothing
                     {
                         SpawnEnemy(randomNum - 6, zSpawn);
-                        hasSpawnedEnemiesTonight = true;
+                        hasSpawnedEnemiesTonight = true;    //Boolean to check that something has been spawned in the round. For the AIManager
                         
                     }
                 }
@@ -136,29 +137,13 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnEnemy(int SpawnX, int SpawnZ)
     {
+        //Instatiates enemy game object
         GameObject spawnedEnemy = Instantiate(EnemiesToSpawn[Random.Range(0, EnemiesToSpawn.Count)].gameObject, new Vector3(GameManager.instance.tileArray[new Vector2Int(SpawnX, SpawnZ)].position.x, 0.0f, GameManager.instance.tileArray[new Vector2Int(SpawnX, SpawnZ)].position.y), Quaternion.identity);
         Debug.Log(spawnedEnemy.GetComponent<AI>().GetGridPosition());
+        GameManager.instance.aiManager.AddAIToList(spawnedEnemy); //Adds it to the AI Manager List
         spawnedEnemy.GetComponent<AI>().SetGridPosition(new Vector2Int((int)spawnedEnemy.transform.position.x, (int)spawnedEnemy.transform.position.z));
-        spawnedEnemy.name = "Enemy " + enemyIDCounter;
-        GameManager.instance.tileArray[new Vector2Int(SpawnX, SpawnZ)].entity = spawnedEnemy.GetComponent<AI>();
-        enemyTax = spawnedEnemy.GetComponent<AI>().GetCost();
-    }
-
-//    while(GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].entity != null && randomiseSpawnCounter<100) 
-//                    {
-//                        randomNum = Random.Range(0, 11);
-//                        randomiseSpawnCounter += 1;
-//                    }
-    //if (GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].entity == null)
-    //{
-    //    spawnedEnemy = Instantiate(EnemiesToSpawn[Random.Range(0, EnemiesToSpawn.Count)].gameObject, new Vector3(GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].position.x, 0.0f, GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].position.y), Quaternion.identity);
-    //    Debug.Log(spawnedEnemy.GetComponent<AI>().GetGridPosition());
-    //    spawnedEnemy.GetComponent<AI>().SetGridPosition(new Vector2Int((int)spawnedEnemy.transform.position.x, (int)spawnedEnemy.transform.position.z));
-    //    spawnedEnemy.name = "Enemy " + enemyIDCounter;
-    //    GameManager.instance.tileArray[new Vector2Int(xSpawn, randomNum - 6)].entity = spawnedEnemy.GetComponent<AI>();
-    //    hasSpawnedEnemiesTonight = true;
-    //    enemyTax = spawnedEnemy.GetComponent<AI>().GetCost();
-    //}
-
-    
+        spawnedEnemy.name = "Enemy " + enemyIDCounter; //Gives it a useful name in the heirarchy
+        GameManager.instance.tileArray[new Vector2Int(SpawnX, SpawnZ)].entity = spawnedEnemy.GetComponent<AI>(); //Makes the enemy occupy the grid Tile entity variable
+        enemyTax = spawnedEnemy.GetComponent<AI>().GetCost(); //Changes the tax variable on the spawner so the correct amount of points is taken away from the budget
+    }    
 }
--- Assets/Scripts/Reese/Managers/GridManager.cs
+++ Assets/Scripts/Reese/Managers/GridManager.cs
@@ -8,13 +8,13 @@ public class GridManager : MonoBehaviour
     {
         //Get the size of the ground and how many grid squares there are based off of it
         //Does not account for (ceb) cell(s) size yet
-        int x = (int)(GameManager.instance.ground.transform.localScale.x * 10);
-        int z = (int)(GameManager.instance.ground.transform.localScale.z * 10);
+        int x = (int)(GameManager.instance.ground.transform.localScale.x * 10)+1;
+        int z = (int)(GameManager.instance.ground.transform.localScale.z * 10)+1;
 
         GameManager.instance.tileArray = new Dictionary<Vector2Int, GridTile>();
-        for (int i = (-x/2); i < x/2; i++)
+        for (int i = -7; i <= x-7; i++) //the grid is 13x13 so that the enemies can spawn on inaccessible terrain
         {
-            for (int j = (-z/2); j < z/2; j++)
+            for (int j = (-7); j <= z-7; j++)
             {
                 var newTile = new GridTile();
                 newTile.name = $"Tile {i}{j}";
@@ -22,7 +22,15 @@ public class GridManager : MonoBehaviour
                 newTile.gridPosition = new Vector2Int(i, j);
                 newTile.traversable = true;
                 GameManager.instance.tileArray[new Vector2Int(i, j)] = newTile;
+                
             }
+        }
+        for(int n = -7; n <=6; n++) //Makes all the edge tiles inaccessible so they cannot be targeted by attacks or moved into
+        {
+            GameManager.instance.tileArray[new Vector2Int(n, 6)].traversable = false;
+            GameManager.instance.tileArray[new Vector2Int(n, -7)].traversable = false;
+            GameManager.instance.tileArray[new Vector2Int(6, n)].traversable = false;
+            GameManager.instance.tileArray[new Vector2Int(-7, n)].traversable = false;
         }
         
     }
